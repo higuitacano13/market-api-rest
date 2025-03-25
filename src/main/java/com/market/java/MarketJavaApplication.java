8c0be868866cd653656d5d8b94135d0e6a7522a6
Juan Higuita <jhiguitac@gmail.com>
2025-03-24 21:25:43 -0500

    Se crea arquitectura de capas basada en dominio

---
+++ src/main/java/com/market/java/MarketJavaApplication.java
@@ -0,0 +1,13 @@
+package com.market.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketJavaApplication.class, args);
+	}
+
+}
