b91143e71e88c83dbff77367de1c251114a4ef72
Juan Higuita <jhiguitac@gmail.com>
2025-03-25 22:20:52 -0500

    Se realiza mapeo de entidades en BD

---
+++ src/main/java/com/market/java/persistence/entities/Producto.java
@@ -0,0 +1,85 @@
+package com.market.java.persistence.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="productos")
+public class Producto {
+
+    @Id // -> Indica que la propiedad corresponde a la clave primaria.
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // -> Indica que la clave primaria es autoincremental
+    @Column(name="id_producto") // -> Se utiliza cuando el nombre del campo es diferente al nombre de la propiedad
+    private Integer idProducto;
+
+    private String nombre;
+
+    @Column(name="id_categoria")
+    private Integer idCategoria;
+
+    @Column(name="codigo_barras")
+    private String codigoBarras;
+
+    @Column(name="precio_venta")
+    private Double precioVenta;
+
+    @Column(name="cantidad_stock")
+    private Integer cantidadStock;
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getCodigoBarras() {
+        return codigoBarras;
+    }
+
+    public void setCodigoBarras(String codigoBarras) {
+        this.codigoBarras = codigoBarras;
+    }
+
+    public Double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(Double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public Integer getCantidadStock() {
+        return cantidadStock;
+    }
+
+    public void setCantidadStock(Integer cantidadStock) {
+        this.cantidadStock = cantidadStock;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+    private Boolean estado;
+}
