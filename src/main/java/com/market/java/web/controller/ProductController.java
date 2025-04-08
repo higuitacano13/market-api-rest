a8011a60eb95d4118c1afacd8a87d9b395639f36
Juan Higuita <jhiguitac@gmail.com>
2025-04-07 20:24:52 -0500

    Se implementa control de respuesta utilizando ResponseEntity

--- src/main/java/com/market/java/web/controller/ProductController.java
+++ src/main/java/com/market/java/web/controller/ProductController.java
@@ -17,34 +17,35 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/all")
-    public List<Product> getAll(){
-        return productService.getAll();
+    public ResponseEntity<List<Product>> getAll(){
+        return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public Optional<Product> getProduct(@PathVariable("id") int productId){
-        return productService.getProduct(productId);
+    public ResponseEntity<Product> getProduct(@PathVariable("id") int productId){
+        return productService.getProduct(productId)
+                .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @GetMapping("/category/{categoryId}")
-    public Optional<List<Product>> getByCategory(@PathVariable("categoryId") int idCategory){
-        return productService.getByCategory(idCategory);
+    public ResponseEntity<List<Product>> getByCategory(@PathVariable("categoryId") int idCategory){
+        return productService.getByCategory(idCategory)
+                .map(products -> new ResponseEntity<>(products, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping("/save")
-    public ResponseEntity<?> save(@RequestBody Product product) {
-        try {
-            Product saved = productService.save(product);
-            return ResponseEntity.ok(saved);
-        } catch (Exception e) {
-            String errorMessage = "Error al guardar el producto: " + e.getMessage();
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
-        }
+    public ResponseEntity<Product> save(@RequestBody Product product) {
+        return new ResponseEntity<>(productService.save(product), HttpStatus.CREATED);
     }
 
     @DeleteMapping("delete/{id}")
-    public boolean delete(@PathVariable("id") int productId){
-        return productService.delete(productId);
+    public ResponseEntity delete(@PathVariable("id") int productId){
+        if(productService.delete(productId)){
+            return new ResponseEntity<>(HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
